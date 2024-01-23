d7c83f2eb1398f8e37bb0eee75b0ed692f8fe048
Zaur Bairamau <bairamau.z@gmail.com>
2024-01-23 18:28:43 +0400

    1.8 Nest secondary definitions

--- 1.8.rkt
+++ 1.8.rkt
@@ -1,17 +1,15 @@
 #lang sicp
 (define (qbrt x)
-  (qbrt-iter 1.0 x))
-
-(define (improve guess x)
+  (define (improve guess x)
   (/ (+ (/ x (* guess guess))
         (* 2 guess))
      3))
-
-(define (qbrt-iter guess x)
+  (define (qbrt-iter guess x)
   (if (< (abs (- (* guess guess guess) x)) 0.0001)
       guess
       (qbrt-iter (improve guess x)  x)
       ))
+  (qbrt-iter 1.0 x))
 
 (qbrt 27)
 (qbrt -27)
