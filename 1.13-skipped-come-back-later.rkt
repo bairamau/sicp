70020a6f910284d39ded0d3029fee2dfa0997ec7
Zaur Bairamau <bairamau.z@gmail.com>
2024-03-09 12:05:24 +0400

    1.13-skipped-come-back-later.rkt

---
+++ 1.13-skipped-come-back-later.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+;Prove that Fib(n)
+;is the closest integer to φ^n/√5
+;where φ=(1+√5)/2
+;Hint: Let ψ=(1-√5)/2
+;Use induction and the definition of the Fibonacci numbers (see 1.2.2) to prove that Fib(n)=(φ^n-ψ^n)/√5
+
+
+;I hope I will come back to this later
